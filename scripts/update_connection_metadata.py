7af6de8fe9c9a3df880f6aeca4913ba4e23ab703
Alec Mitchell <alecpm@gmail.com>
2016-11-04 09:15:30 -0700

    Add cache minimize call to avoid excessive RAM usage.

--- scripts/update_connection_metadata.py
+++ scripts/update_connection_metadata.py
@@ -31,6 +31,7 @@ if __name__ == '__main__':
         if total % 100 == 0:
             transaction.commit()
             print "Migrating ... {} connections".format(total)
+            app.p_jar.cacheMinimize()
 
     transaction.commit()
     print "Migrated {} total connections.".format(total)
